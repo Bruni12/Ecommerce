c00a4c8e13384d8f8333ae0caf1ebca8fe67d5c6
Bruni12 <brunnetteboy@gmail.com>
2019-11-05 12:18:29 -0700

    built shop seachbar form and form field

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
+import ShopSearchBar from './shopSearchBar';
 
 class Shop extends Component {
 
@@ -15,24 +16,27 @@ class Shop extends Component {
         ]
         this.props.setHeaderLinks(headerLinks);
         this.props.fetchShopCategories();
-                
+
         // filter products with links
         this.props.fetchShopProducts();
     }
 
-     shouldComponentUpdate(nextProps) {
+    shouldComponentUpdate(nextProps) {
         if(this.props != nextProps) {
             this.props.setNavbarLinks(nextProps.categories, (_id) => this.props.filterProductsWithCategoryId(_id));
         }
         return true
     }
 
+    onSubmit = (fields) => {
+        console.log(fields);
+    }
+
     render() {
 
         return (
             <div className='shop'>
-                {/* shop search bar */}
-                {/* shop product */}
+                <ShopSearchBar onSubmit={this.onSubmit} className='shop__search-bar'/>
                 <div className='shop__products'>
                     {
                         this.props.filteredProducts.map(product => {
@@ -56,10 +60,10 @@ class Shop extends Component {
 }
 
 function mapStateToProps(state) {
-    const {categories, filteredProducts } = state.shop;
-    return { 
-       categories,
-       filteredProducts
+    const { categories, filteredProducts } = state.shop;
+    return {
+        categories,
+        filteredProducts
     } 
 }
 
